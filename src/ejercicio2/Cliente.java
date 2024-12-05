ca38aa2715efcd172eafbcd90c6beffccd8e3f5e
Snaider <snaiderviracocha@gmail.com>
2024-12-05 14:10:19 +0100

    Ejercio 2 finalizado

--- src/ejercicio2/Cliente.java
+++ src/ejercicio2/Cliente.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Cliente {
 
@@ -17,15 +20,20 @@ public class Cliente {
 
 		try {
 			socket = new Socket(ipServer, puertoServer);
-
+			
 			System.out.println("Conexión realizada con el servidor");
 			objectInputStream = new ObjectInputStream(socket.getInputStream());
 			Persona p = (Persona) objectInputStream.readObject();
 			p.setNombre("Carmen");
-
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			Date fecha = format.parse("1983-01-01");
+			p.setFechaNacimiento(fecha);
+			
 			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 			objectOutputStream.writeObject(p);
 			objectOutputStream.flush();
+		} catch (ParseException e) {
+			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
--- src/ejercicio2/Servidor.java
+++ src/ejercicio2/Servidor.java
@@ -5,6 +5,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Servidor {
 
@@ -19,24 +22,32 @@ public class Servidor {
 		try {
 			serverSocket = new ServerSocket(puertoServer, 10000);
 			socket = serverSocket.accept();
-			
+
 			Persona p = new Persona();
 			p.setNif("16078123X");
 			p.setNombre("Pepito");
 			p.setApellido("Grillo");
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            Date fecha = format.parse("1990-12-12");
+            p.setFechaNacimiento(fecha);
 			p.setGenero('M');
-			
+
 			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 			objectOutputStream.writeObject(p);
 			objectOutputStream.flush();
-			
+
+			Thread.sleep(1000);
 			objectInputStream = new ObjectInputStream(socket.getInputStream());
 			System.out.println(objectInputStream.readObject());
-			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		
 		} finally {
 			try {
 				if (null != objectOutputStream)
